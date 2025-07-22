036bf0db63dbe942c088371a5e4f532b21c849e9
mid <mid@k302.kcsk.ac.jp>
2025-07-22 11:43:45 +0900

    niino

--- src/DVD_/MainFrame.java
+++ src/DVD_/MainFrame.java
@@ -1,5 +1,35 @@
 package DVD_;
 
-public class MainFrame {
+import java.awt.CardLayout;
 
+import javax.swing.JPanel;
+
+
+public class MainFrame {
+	   private CardLayout cardLayout;
+	    private JPanel cardPanel;
+
+	    public MainFrame() {
+	        setTitle("図書館システム");
+	        setDefaultCloseOperation(EXIT_ON_CLOSE);
+	        setSize(600, 400);
+
+	        cardLayout = new CardLayout();
+	        cardPanel = new JPanel(cardLayout);
+
+	        // すべての機能パネルを登録
+	        cardPanel.add(new TopPanel(this), "TOP");
+	        cardPanel.add(new BookPanel(this), "BOOK");
+	        cardPanel.add(new MemberPanel(this), "MEMBER");
+	        cardPanel.add(new LendPanel(this), "LEND");
+	        cardPanel.add(new ReturnPanel(this), "RETURN");
+	        cardPanel.add(new ListPanel(this), "LIST");
+
+	        add(cardPanel);
+	        setVisible(true);
+	    }
+
+	    public void showPanel(String name) {
+	        cardLayout.show(cardPanel, name);
+	    }
 }
