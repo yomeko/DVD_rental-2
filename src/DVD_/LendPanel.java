06ffcbcfd2cea20588fa1a5086c09b382e9584eb
mid <mid@k302.kcsk.ac.jp>
2025-07-22 11:40:15 +0900

    testadd

--- src/DVD_/LendPanel.java
+++ src/DVD_/LendPanel.java
@@ -12,22 +12,22 @@ public class LendPanel extends JPanel {
 	public LendPanel(MainFrame frame) {
         //ボタンとかフィールド作成
     	setLayout (new GridLayout(3, 2));
-    	JTextField memberField = new JTextField();
-    	JTextField bookCodeField = new JTextField();
+    	JTextField idField = new JTextField();
+    	JTextField CodeField = new JTextField();
     	JButton lendBtn=new JButton("貸出");
     	JButton backBtn=new JButton("TOPへ戻る");
     	//DB実行
     	lendBtn.addActionListener(e -> {
-        	DB.lenddvd(memberField.getText(),( bookCodeField.getText()));
+        	DB.lenddvd(idField.getText(),( CodeField.getText()));
         	JOptionPane.showMessageDialog(this, "貸出しました。");
         	});
         //TOP戻る
     	backBtn.addActionListener(e -> frame.showPanel("TOP"));
         //部品追加
     	add(new JLabel("会員ID"));
-    	add(memberField);
-    	add(new JLabel("書籍コード"));
-    	add(bookCodeField);
+    	add(idField);
+    	add(new JLabel("DVDコード"));
+    	add(CodeField);
     	add(lendBtn);
     	add(backBtn);
     }
