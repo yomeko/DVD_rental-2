8c925630910b08b457a4997afec95994ea98816f
mid <mid@k302.kcsk.ac.jp>
2025-07-23 11:29:56 +0900

    157

--- src/DVD_/ListPanel.java
+++ src/DVD_/ListPanel.java
@@ -4,11 +4,11 @@ import java.awt.BorderLayout;
 import java.util.List;
 
 import javax.swing.JButton;
-import javax.swing.JFrame;
+import javax.swing.JPanel;
 import javax.swing.JScrollPane;
 import javax.swing.JTextArea;
 
-public class ListPanel extends JFrame{
+public class ListPanel extends JPanel{
     public ListPanel(MainFrame frame) {
         //レイアウト追加
         setLayout(new BorderLayout());
