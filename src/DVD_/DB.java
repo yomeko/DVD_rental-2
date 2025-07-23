6cf38eb480a54ea30ca02e09e0f942d09439ff5b
mid <mid@k302.kcsk.ac.jp>
2025-07-23 11:37:09 +0900

    oreore

--- src/DVD_/DB.java
+++ src/DVD_/DB.java
@@ -11,13 +11,13 @@ import java.util.List;
 
 public class DB {
 	//DBアクセス＆ユーザー名とパスワード
-	
+
 	private static final String URL = "jdbc:mysql://localhost/dvd_shop";
 	private static final String USER ="root";
 	private static final String PASS ="";
-	
+
 	//会員の登録
-	
+
 	public static void insertMember(String id,String name) {
 		try(Connection conn =DriverManager.getConnection(URL,USER,PASS);
 				PreparedStatement ps = conn.prepareStatement("INSERT INTO member(id,name) VALUES(?,?)")){
@@ -30,7 +30,7 @@ public class DB {
 	}
 
 	//DVDの登録
-	
+
 	public static void insertDVD(String code,String title) {
 		try(Connection conn =DriverManager.getConnection(URL,USER,PASS);
 				PreparedStatement ps = conn.prepareStatement("INSERT INTO dvd(code,title,is_lent) VALUES(?,?,false)")){
@@ -42,8 +42,8 @@ public class DB {
 		}
 	}
 
-	//DVDを貸出状態に更新する処理 
-	
+	//DVDを貸出状態に更新する処理
+
 	public static void lendDVD(String id, String code) {
 		try(Connection conn =DriverManager.getConnection(URL,USER,PASS);
 				PreparedStatement ps = conn.prepareStatement("UPDATE dvd set is_lent =true WHERE code=?;")){
@@ -53,9 +53,9 @@ public class DB {
 			e.printStackTrace();
 		}
 	}
-	
+
 	//DVDを返却済み状態に更新する処理
-	
+
 	public static void returnDVD(String code) {
 		try(Connection conn =DriverManager.getConnection(URL,USER,PASS);
 				PreparedStatement ps = conn.prepareStatement("UPDATE dvd set is_lent =false WHERE code=?;")){
@@ -64,16 +64,16 @@ public class DB {
 		}catch(SQLException e) {
 			e.printStackTrace();
 		}
-		
+
 	}
 
 	//貸し出し中のDVDリスト
-	
+
 	public static List<String> getLentDVDs(){
 		List<String> dvds = new ArrayList<>();
 		try(Connection conn =DriverManager.getConnection(URL,USER,PASS);
 				Statement st = conn.createStatement();
-				ResultSet rs = st.executeQuery("SELECT code,title FROM book WHERE is_lent = true")){
+				ResultSet rs = st.executeQuery("SELECT code,title FROM dvd WHERE is_lent = true")){
 			while(rs.next()) {
 				dvds.add(rs.getString("code")+"-"+rs.getString("title"));
 			}
